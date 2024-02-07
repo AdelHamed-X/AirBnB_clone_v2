79c061a7204575eea5d7e7a59aaa64703fbae128
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-07 03:57:35 +0200

    Adding Task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This fabric script does the follwing stuff:
+    - Creates versions folder if it doesn't exist
+    - Creates .tgz file of all the web_static folder components """
+
+from fabric.api import local
+
+
+def creaets_folder():
+    """ creates versions folder """
+    local("mkdir -p versions")
+
+def file_name():
+    return str(local("date +'%Y%m%d%H%M%S'"))
+
+def compress_all():
+    """ creates an archive compressed file """
+    file = "web_static_{}".format(file_name())
+    local(f"tar -cvzf versions/{file}.tgz web_static/*")
+
+def do_pack():
+    creaets_folder()
+    compress_all()
