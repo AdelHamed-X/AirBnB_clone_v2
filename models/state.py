bb839aae1d8a484ac2db0ec020d8f3997799a093
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-19 23:56:30 +0200

    Updating the state class for task 7

--- models/state.py
+++ models/state.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
 import models
 from models.city import City
+from os import environ
 
 
 class State(BaseModel, Base):
@@ -13,17 +14,18 @@ class State(BaseModel, Base):
     Attributes:
         name: input name
     """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
-
-    @property
-    def cities(self):
-        all_cities = models.storage.all(City)
-        city_list = []
-        for elem in all_cities.values():
-            if (elem.state_id == self.id):
-                city_list.append(elem)
-        return (city_list)
+    if environ.get('HBNB_STORAGE_TYPE') == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade='all, delete, delete-orphan',
+                            backref="state")
+    else:
+        @property
+        def cities(self):
+            all_cities = models.storage.all(City)
+            city_list = []
+            for elem in all_cities.values():
+                if (elem.state_id == self.id):
+                    city_list.append(elem)
+            return (city_list)
     
