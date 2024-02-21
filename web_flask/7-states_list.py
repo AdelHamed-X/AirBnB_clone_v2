7f4040eb65e06c7dc400dffc0d35701d5efc50dc
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-21 13:02:22 +0200

    Adding Task 8 9

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,15 +10,14 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def app_teardown():
+def app_teardown(exception):
     storage.close()
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    all_states = storage.all(State)
-    render_template('7-states_list.html', all_states=all_states)
-
+    all_states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', all_states=all_states)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
