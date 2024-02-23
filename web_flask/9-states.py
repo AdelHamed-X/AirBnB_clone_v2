327ad23f9222901c9487adb31254bee880198b51
AdelHamed-X <adelhamed349.ah@gmail.com>
2024-02-23 16:47:00 +0200

    Fixing Task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+""" A Flask script """
 
-from flask import Flask, render_template
 from models import *
-from models import storage
+from flask import Flask, render_template
+from models.state import State
+
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<state_id>', strict_slashes=False)
-def states(state_id=None):
-    """display the states and cities listed in alphabetical order"""
-    states = storage.all("State")
-    if state_id is not None:
-        state_id = 'State.' + state_id
-    return render_template('9-states.html', states=states, state_id=state_id)
+app.route('/states', strict_slashes=False)
+app.route('/state/<int:id>')
+def show_states(id):
+    """ function to handle /states page """
+    all_states = storage.all(State).values()
+    return render_template('9-states.html', all_states=all_states, id=id)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+app.teardown_appcontext
+def teardown(exception):
+    """ Reloading the session """
     storage.close()
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host="0.0.0.0", port=5000, debug=True)
